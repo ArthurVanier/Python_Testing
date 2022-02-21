8f0aa077f171a3dd6f77912d3e7d99432ac8e3f6
Arthur Vanier <arthur.vanier@epitech.eu>
2022-02-21 15:12:18 +0100

    update test

--- test.py
+++ test.py
@@ -5,10 +5,12 @@ def test_show_summary(client):
     response = client.post("/showSummary", data={"email": "admin@irontemple.com"})
     assert response.status_code == 200
 
-    response = client.post("/showSummary", data={"email": ""})
-    assert response.status_code == 400
-
     response = client.post("/showSummary", data={"email": "wrongEmail@email.fr"})
     assert response.status_code == 400
+
+    response = client.post("/showSummary", data={})
+    assert response.status_code == 400
+
+    
 
     
