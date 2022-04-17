7352794651cf23e0d399c5307cd4ec6865ea658c
ArthurVanier <arthur.vanier@epitech.eu>
2022-04-17 08:29:23 +0200

    osef

--- conftest.py
+++
@@ -1,7 +0,0 @@
-from .server import app as app_server
-import pytest
-
-@pytest.fixture
-def client():
-    with app_server.test_client() as client:
-        yield client
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import json
 from time import strptime
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
 from datetime import datetime, timedelta, time
 
 
@@ -24,14 +24,12 @@ clubs = loadClubs()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html'), 200
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
     try:
         club = [club for club in clubs if club['email'] == request.form['email']][0]
-        
-        
         return render_template('welcome.html', club=club,competitions=competitions, clubs=clubs), 200
     except:
         print("Sorry, that email wasn't found.")
@@ -40,13 +38,15 @@ def showSummary():
 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
+    foundClub = [c for c in clubs if c['name'] == club]
+    foundCompetition = [c for c in competitions if c['name'] == competition]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        foundClub = foundClub[0]
+        foundCompetition = foundCompetition[0]
+        return render_template('booking.html',club=foundClub,competition=foundCompetition), 200
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=club, competitions=competitions), 400
 
 
 @app.route('/purchasePlaces',methods=['POST'])
@@ -59,7 +59,7 @@ def purchasePlaces():
         flash("You can't purchase more than 12 places")
         return render_template('welcome.html', club=club, competitions=competitions), 400
 
-    if placesRequired > int(club['points']):
+    if placesRequired * 3 > int(club['points']):
         flash("You can't purchase more places than point you have")
         return render_template('welcome.html',club=club, competitions=competitions), 400
     
@@ -67,12 +67,14 @@ def purchasePlaces():
         flash("You can't purchase more this number of place")
         return render_template('welcome.html',club=club, competitions=competitions), 400
     
+    print(datetime.strptime(competition['date'], "%Y-%m-%d %H:%M:%S"))
+    print(datetime.now())
     if datetime.strptime(competition['date'], "%Y-%m-%d %H:%M:%S") <  datetime.now():
         flash("You can't purchase places for a competition wich  already happen")
         return render_template('welcome.html',club=club, competitions=competitions), 400
         
     competition['numberOfPlaces'] = str(int(competition['numberOfPlaces'])-placesRequired)
-    club['points'] = str(int(club['points']) - placesRequired)
+    club['points'] = str(int(club['points']) - (placesRequired * 3))
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions), 200
 
